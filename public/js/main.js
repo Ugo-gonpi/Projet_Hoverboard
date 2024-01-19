436ee3595d7c6a772b57ba820a1198f795d5b489
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-19 15:12:17 +0100

    created the base of the exercise and its js

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+let container = document.getElementById('container')
+let colors = ["#10258e", "#38323a", "#1a1723", "#763e21", "#3f9de2", "#c794a3"]
+let SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    let square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+   let color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
